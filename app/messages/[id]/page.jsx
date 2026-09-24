b1ee30145165a3af863142e0a0eae909d92ecb55
jikanbako <jikanbako3090@gmail.com>
2026-09-24 12:12:25 +0100

    Add files via upload

--- app/messages/[id]/page.jsx
+++ app/messages/[id]/page.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'next/navigation';
-import { supabase } from '../../../lib/supabaseClient';
+import { supabase } from '@/lib/supabaseClient';
 
 export default function ConversationPage() {
   const { id } = useParams();
